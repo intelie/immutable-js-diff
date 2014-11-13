020071ef3feda9b9cec950c8e7526dfac695b2c2
Breno Ferreira <bferreira@windowslive.com>
2014-11-13 15:14:59 -0200

    Added path escape/unescape/concat functions

---
+++ src/path.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var slashRe = new RegExp('/', 'g');
+var escapedSlashRe = new RegExp('~1', 'g');
+var tildeRe = /~/g;
+var escapedTildeRe = /~0/g;
+
+var Path = {
+  escape: function (str) {
+    if(typeof(str) !== 'string'){
+      throw 'param str (' + str + ') is not a string';
+    }
+
+    return str.replace(tildeRe, '~0').replace(slashRe, '~1');
+  },
+
+  unescape: function (str) {
+    if(typeof(str) == 'string') {
+      return str.replace(escapedSlashRe, '/').replace(escapedTildeRe, '~');
+    }
+    else {
+      return str;
+    }
+  },
+  concat: function(path, key){
+    return path + '/' + key;
+  }
+};
+
+module.exports = Path;
---
+++ tests/path.test.js
@@ -0,0 +1,40 @@
+'use strict';
+
+var assert = require('assert');
+var path = require('../src/path');
+
+describe('Path', function() {
+  it('escape / in string', function () {
+    var str = 'prop/prop';
+    var expected = 'prop~1prop';
+
+    var result = path.escape(str);
+
+    assert.strictEqual(result, expected);
+  });
+
+  it('unescape / in string', function () {
+    var str = 'prop~1prop';
+    var expected = 'prop/prop';
+
+    var result = path.unescape(str);
+
+    assert.strictEqual(result, expected);
+  });
+
+  it('reverts to original string', function () {
+    var str = 'prop/prop';
+
+    var result = path.unescape(path.escape(str));
+
+    assert.strictEqual(result, str);
+  });
+
+  it('returns same string when escaping is not necessary', function() {
+    var str = 'normalstring';
+
+    var result = path.escape(str);
+
+    assert.strictEqual(result, str);
+  });
+});
