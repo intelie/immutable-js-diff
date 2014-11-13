1432f42bfdc29d4de16eebaf9aa7c8ae420991e4
Breno Ferreira <bferreira@windowslive.com>
2014-11-13 17:50:03 -0200

    Initial code for performance test

---
+++ tests/performance/immutableDiff.perftest.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var Immutable = require('immutable');
+var veryBigArray = require('./veryBigArray');
+var immutableDiff = require('../../src/diff');
+var jsondiff = require('jsondiff');
+
+var immutableDiffPerformanceTest = function(){
+  var list1 = Immutable.fromJS(veryBigArray);
+  var list2 = list1.concat({x: 10, y: 7000});
+
+  var diff = immutableDiff(list1, list2);
+};
+
+var jsondiffPerformanceTest = function(){
+  var list1 = veryBigArray;
+  var list2 = list1.concat({x: 10, y: 7000});
+
+  var diff = jsondiff(list1, list2);
+};
+
+immutableDiffPerformanceTest();
